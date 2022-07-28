f7a628582ed0bbc96eb92aac987adb42afb1c4e7
Hasnain-Shabbir <hasnainshabbir186@gmail.com>
2022-07-28 07:12:33 +0500

    first commit

---
+++ src/components/components.js
@@ -0,0 +1,12 @@
+export { default as BodyPart } from './BodyPart/BodyPart';
+export { default as Detail } from './Detail/Detail';
+export { default as ExerciseCard } from './ExerciseCard/ExerciseCard';
+export { default as Exercises } from './Exercises/Exercises';
+export { default as ExerciseVideos } from './ExerciseVideos/ExerciseVideos';
+export { default as Footer } from './Footer/Footer';
+export { default as HeroBanner } from './HeroBanner/HeroBanner';
+export { default as HorizontalScrollbar } from './HorizontalScrollbar/HorizontalScrollbar';
+export { default as Loader } from './Loader/Loader';
+export { default as Navbar } from './Navbar/Navbar';
+export { default as SearchExercise } from './SearchExercise/SearchExercise';
+export { default as SimilarExercise } from './SimilarExercise/SimilarExercise';
